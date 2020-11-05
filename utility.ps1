7e094fe002e058a0b9525e80ff5e675f51841e12
lmkelly <liamkelly.m@gmail.com>
2020-11-04 22:32:45 -0500

    Added 8.1 ansible files and changes to utility

--- utility.ps1
+++ utility.ps1
@@ -5,17 +5,12 @@ param(
 	[string]$type,
 
 	[Alias("host")]
-	[Parameter (Mandatory=$false)]
+	[Parameter (Mandatory=$true)]
 	[string]$VMN
 	
 )
 
-
 function what() {
-	if ($type -eq $null) {
-		$type = read-host -Prompt "Would you like to [C]hange a VM network adapter, [G]et the IP of a VM (Ansible Format) [E]xit?"
-	}
-
 	if ($type -match "^[cC]$") {
 		$vms = Get-VM
 		$vms.name
@@ -41,15 +36,22 @@ function what() {
 			$vms.name
 			$VMN = read-host -Prompt "Which VM would you like to work with?"
 		}
+		
 		getIP $VMN
 	}
-	elseif ($type -match "^[fF]$") {
-		fClone
+	elseif ($type -match "^[mM]$") {
+		if ($VMN -eq $null) {
+			$vms = Get-VM
+			$vms.name
+			$VMN = read-host -Prompt "Which VM would you like to work with?"
+		}
+		getMac $VMN
 	}
 	elseif ($type -match "^[eE]$") {
 		exit
 	}
 	else {
+		$type = read-host -Prompt "Would you like to [C]hange a VM network adapter, [G]et the IP of a VM (Ansible Format) or Get [M]ac address of VM? [E]xit?"
 		what
 	}
 }
@@ -62,11 +64,14 @@ function getIP([string] $vmName) {
 	get-vm -name $vmName | foreach-object { write-host $_.Guest.IPAddress[0] "hostname=$_"} 
 }
 
+function getMac([string] $vmName) {
+	get-vm -name $vmName | Get-NetworkAdapter | Select Name,MacAddress
+}
+
 $c = $defaultviserver
 if ($c -eq $null) {
 	$viServer = read-host -Prompt "Enter vCenter hostname/ip address"
 	Connect-VIServer -Server $viServer
 }
-
 
 what
